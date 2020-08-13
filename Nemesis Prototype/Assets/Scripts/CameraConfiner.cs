21ef6fd42010967796df500bdb4bd29993c09edc
César Neves Mosca <Cronokami@hotmail.com>
2020-08-13 18:07:02 -0300

    Projeto com movimentação

---
+++ Nemesis Prototype/Assets/Scripts/CameraConfiner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraConfiner : MonoBehaviour
+{
+	private CinemachineFreeLook freeLook;
+
+	public float maxAngle;
+	public float lerpCameraBack;
+	
+    void Start()
+    {
+		freeLook = GetComponent<CinemachineFreeLook>();
+    }
+	
+
+    void Update()
+    {
+		if (freeLook.m_XAxis.Value > maxAngle)
+		{
+			freeLook.m_XAxis.Value = Mathf.Lerp(freeLook.m_XAxis.Value, maxAngle, lerpCameraBack);
+		}
+		else if (freeLook.m_XAxis.Value < -maxAngle)
+		{
+			freeLook.m_XAxis.Value = Mathf.Lerp(freeLook.m_XAxis.Value, -maxAngle, lerpCameraBack);
+		}
+	}
+}
---
+++ Nemesis Prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public GameObject cam;
+	public bool follow;
+
+	private Vector3 startingPos;
+
+	private void Start()
+	{
+		transform.parent = cam.transform;
+		transform.rotation = cam.transform.rotation;
+		startingPos = transform.localPosition;
+	}
+
+	private void Update()
+	{
+		if (follow)
+		{
+			transform.parent = cam.transform;
+			transform.localPosition = Vector3.Lerp(transform.localPosition, startingPos, 0.5f);
+			transform.rotation = Quaternion.Slerp(transform.rotation, cam.transform.rotation, 0.2f);
+		}
+		else
+		{
+			transform.parent = null;
+		}
+	}
+
+}
