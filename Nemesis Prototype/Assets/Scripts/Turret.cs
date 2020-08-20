c1c07ad59e58a07bb8efe4f0c442dc64d72dc9da
César Neves Mosca <Cronokami@hotmail.com>
2020-08-20 12:52:12 -0300

    Turret aponta para o player

---
+++ Nemesis Prototype/Assets/Scripts/Turret.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+	public Transform player;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(player.position.x, -player.position.y, player.position.z) - transform.position, Vector3.up), 0.5f);
+    }
+}
