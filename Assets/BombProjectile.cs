97ae09472f5705a81e3280c648943a909c02a22a
João Vitor <jovitor86@gmail.com>
2020-08-27 17:10:51 -0300

    Bomb projectile

---
+++ Assets/BombProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombProjectile : MonoBehaviour
+{
+    public ParticleSystem explosionEffects;
+    private bool hitted;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!hitted)
+        {
+            Instantiate(explosionEffects, transform.position, transform.rotation);
+            GetComponent<SphereCollider>().enabled = true;
+            Destroy(gameObject, 5f);
+            hitted = true;
+        }
+    }
+
+
+
+}
