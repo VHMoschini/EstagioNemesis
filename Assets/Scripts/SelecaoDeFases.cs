b413393d8017bb52e01b3e42bf95cb39b8003ec4
César Neves Mosca <Cronokami@hotmail.com>
2020-08-27 18:27:30 -0300

    Menu básico de seleção de fases

---
+++ Assets/Scripts/SelecaoDeFases.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelecaoDeFases : MonoBehaviour
+{
+	public void VaiPraFase(int numero)
+	{
+		switch (numero)
+		{
+			case 1:
+				SceneManager.LoadScene("Mapa1");
+				break;
+
+			case 2:
+				SceneManager.LoadScene("Mapa2");
+				break;
+
+			case 3:
+				SceneManager.LoadScene("Mapa3");
+				break;
+
+			default:
+				SceneManager.LoadScene(0);
+				break;
+		}
+		
+
+	}
+}
