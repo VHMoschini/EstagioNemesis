27701508649bbe136208fa7280a2d5ba74dfed45
César Neves Mosca <Cronokami@hotmail.com>
2020-08-30 02:54:54 -0300

    camera testável pelo unity e menos sensível

--- Assets/Scripts/CinemachineCoreGetInputTouchAxis.cs
+++ Assets/Scripts/CinemachineCoreGetInputTouchAxis.cs
@@ -5,13 +5,28 @@ using Cinemachine;
 
 public class CinemachineCoreGetInputTouchAxis : MonoBehaviour
 {
-	public float TouchSensitivity_x = 10f;
-	public float TouchSensitivity_y = 10f;
-
+	public float TouchSensitivity_x;
+	public float TouchSensitivity_y;
+	private CinemachineFreeLook cmFL;
+	public float speed;
 	
 	void Start()
 	{
 		CinemachineCore.GetInputAxis = HandleAxisInputDelegate;
+		cmFL = GetComponent<CinemachineFreeLook>();
+		cmFL.m_XAxis.m_MaxSpeed = speed;
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			cmFL.m_XAxis.m_MaxSpeed = speed;
+		}
+		else
+		{
+			cmFL.m_XAxis.m_MaxSpeed = 0;
+		}
 	}
 
 	float HandleAxisInputDelegate(string axisName)
@@ -27,10 +42,9 @@ public class CinemachineCoreGetInputTouchAxis : MonoBehaviour
 				}
 				else
 				{
-					return 0;
-				//	return Input.GetAxis(axisName);
+					return Input.GetAxis(axisName);
 				}
-/*
+
 			case "Mouse Y":
 				if (Input.touchCount > 0)
 				{
@@ -40,7 +54,7 @@ public class CinemachineCoreGetInputTouchAxis : MonoBehaviour
 				{
 					return Input.GetAxis(axisName);
 				}
-*/
+
 			default:
 				Debug.LogError("Input <" + axisName + "> not recognyzed.", this);
 				break;
