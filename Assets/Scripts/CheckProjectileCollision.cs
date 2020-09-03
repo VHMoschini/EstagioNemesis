b17bbc52c4fbafc7e7609e9bebcdd219fb1f78d1
César Neves Mosca <Cronokami@hotmail.com>
2020-09-03 06:35:02 -0300

    Partículas básicas de madeira

--- Assets/Scripts/CheckProjectileCollision.cs
+++ Assets/Scripts/CheckProjectileCollision.cs
@@ -8,11 +8,14 @@ public class CheckProjectileCollision : MonoBehaviour
     [HideInInspector]public Slider heroBar;
     [HideInInspector]public Slider mercenaryBar;
 
+	public GameObject SplashWoodParticle;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<EnemyCube>())
         {
             heroBar.value++;
+			Instantiate(SplashWoodParticle, collision.transform.position, collision.transform.rotation);
         } else if (collision.gameObject.GetComponent<AllyCube>())
         {
             mercenaryBar.value++;
---
+++ Assets/Scripts/TimerToSelfdestruct.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerToSelfdestruct : MonoBehaviour
+{
+	public float time;
+
+	private void Start()
+	{
+		StartCoroutine(Selfdestruct());
+	}
+
+
+	private IEnumerator Selfdestruct()
+	{
+		yield return new WaitForSeconds(time);
+
+		Destroy(gameObject);
+	}
+}
