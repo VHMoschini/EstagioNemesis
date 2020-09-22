44a378763ad2340336f7104e0249ab5ce4b4b2d9
César Neves Mosca <Cronokami@hotmail.com>
2020-09-22 04:24:03 -0300

    Feedback para que munição está ativa no momento

---
+++ Assets/Scripts/AmmoHighlight.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoHighlight : MonoBehaviour
+{
+	public GameObject[] Ammos;
+
+	public void ChangePosition(int index)
+	{
+		transform.position = Ammos[index].transform.position;
+	}
+}
