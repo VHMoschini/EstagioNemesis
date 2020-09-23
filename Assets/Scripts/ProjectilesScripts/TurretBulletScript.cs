024a3303a0a715317562f1189813199ab3077cc5
João Vitor <jovitor86@gmail.com>
2020-09-22 21:07:02 -0300

    turret atirando

---
+++ Assets/Scripts/ProjectilesScripts/TurretBulletScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBulletScript : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>())
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour
+{
+    private bool canShoot = true;
+    public float bulletVel = 75f;
+    public Transform player;
+    public GameObject bullet;
+    public GameObject muzzle;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(player.position.x, -player.position.y, player.position.z) - transform.position, Vector3.up), 0.5f);
+        transform.LookAt(player.transform);
+        if(canShoot)
+        StartCoroutine(Shoot());
+    }
+
+    IEnumerator Shoot()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(5F);
+        GameObject bulletInst = Instantiate(bullet, muzzle.transform.position, muzzle.transform.rotation);
+        bulletInst.GetComponent<Rigidbody>().AddForce(transform.forward * bulletVel, ForceMode.Impulse);
+        canShoot = true;
+
+    }
+}
