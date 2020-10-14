eed1166031f6fc4b5ba61df865acf50f4f59c631
César Neves Mosca <Cronokami@hotmail.com>
2020-10-14 05:19:14 -0300

    Blocos Aliados tbm deixam restos

--- Assets/Scripts/AllyCube.cs
+++ Assets/Scripts/AllyCube.cs
@@ -7,18 +7,34 @@ public class AllyCube : MonoBehaviour
     private bool isGrounded;
     private Vector3 startingPos;
 
-    private void Start()
+	public float timerToDie;
+	public GameObject debris;
+
+	
+	private void Start()
     {
         startingPos = transform.position;
-
-    }
+		debris = Resources.Load<GameObject>("Debris");
+	}
     private void OnCollisionEnter(Collision collision)
     {
         if (Vector3.Distance(startingPos, transform.position) > 1 && collision.collider.CompareTag("Ground"))
         {
             GetComponentInParent<BarsManager>().hittedAllyCubes++;
             isGrounded = true;
-        }
+			StartCoroutine(TurnToRubble(collision.GetContact(0).point.y));
+		}
 
     }
+
+	private IEnumerator TurnToRubble(float hitPos)
+	{
+		yield return new WaitForSeconds(timerToDie);
+		if (isGrounded)
+		{
+			GetComponentInParent<BarsManager>().hittedEnemieCubes++;
+			Instantiate(debris, new Vector3(transform.position.x, hitPos, transform.position.z), Quaternion.identity, null);
+			Destroy(gameObject);
+		}
+	}
 }
