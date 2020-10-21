3b79b45d523a00460635f390cefbb893471856a7
João Vitor <jovitor86@gmail.com>
2020-10-21 15:51:19 -0300

    fase bonus

---
+++ Assets/Scripts/FaseBonus/EndlessRunnerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessRunnerManager : MonoBehaviour
+{
+    public GameObject[] floors;
+    public float finalZpos;
+    public float initialZpos;
+    public float vel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < floors.Length; i++)
+        {
+            floors[i].transform.position += new Vector3(0,0,-vel);
+            if (floors[i].transform.position == new Vector3(0, 0, finalZpos))
+            {
+                floors[i].transform.position = new Vector3(0, 0, initialZpos);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/FaseBonus/EnemyMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxX = 16;
+    private float actualPosX;
+    private float randomTimeToChangePos;
+    public int posVariation = 1;
+
+
+    void Start()
+    {
+        randomTimeToChangePos = Random.Range(0.1f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        randomTimeToChangePos -= Time.deltaTime;
+        if(randomTimeToChangePos <= 0)
+        {
+            ChangePos();
+            randomTimeToChangePos = Random.Range(1f, 3f);
+        }
+        actualPosX = transform.position.x;
+
+    }
+
+
+    public void ChangePos()
+    {
+        float random = Random.value;
+        float deltaPos = 0f;
+        if (random > 0.5f)
+        {
+            deltaPos = posVariation;
+        }
+        else if (random < 0.5f)
+        {
+            deltaPos = -posVariation;
+        }else if (transform.position.x == 16)
+        {
+            deltaPos = -posVariation;
+
+        }else if (transform.position.x == -16)
+        {
+            deltaPos = posVariation;
+
+        }
+
+        transform.position = new Vector3(actualPosX + deltaPos, transform.position.y, transform.position.z); 
+    }
+}
