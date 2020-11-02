329c078681d845c0cb16bfc886767e98eb00023d
João Vitor <jovitor86@gmail.com>
2020-11-02 18:40:24 -0300

    Configuracoes funcionando

---
+++ Assets/Scripts/ConfigsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class ConfigsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider generalSoundSlider;
+    public Slider sfxSlider;
+    public Slider musicSlider;
+
+    public AudioMixer sfx;
+    public AudioMixer music;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = generalSoundSlider.value;
+        sfx.SetFloat("VolumeMaster(SFX)", sfxSlider.value);
+        music.SetFloat("VolumeSounds", musicSlider.value);
+
+    }
+}
