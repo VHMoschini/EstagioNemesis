73de6ccf837bd815af98a3b0147715d7aef0d57e
César Neves Mosca <Cronokami@hotmail.com>
2020-11-01 21:06:26 -0300

    Sistema de Carrossel no menu gral, em vez de nos personagens

---
+++ Assets/Scripts/MenuSelectorManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSelectorManager : MonoBehaviour
+{
+	public List<GameObject> individualPanel = new List<GameObject>();
+	int index;
+
+	public GameObject panels;
+	private bool manualMove;
+	public bool stopMoving;
+	private float oldMousePosition;
+
+	public Image[] buttons;
+	public Sprite[] buttonsOn;
+	public Sprite[] buttonsOff;
+
+	private void Start()
+	{
+		index = 1;
+		stopMoving = false;
+	}
+
+	private void Update()
+	{
+		if (!manualMove)
+		{
+			panels.transform.localPosition = Vector3.Lerp(panels.transform.localPosition, -individualPanel[index].transform.localPosition, 0.2f);
+		}
+
+		for (int i = 0; i < buttons.Length; i++)
+		{
+			buttons[i].sprite = index == i ? buttonsOn[i] : buttonsOff[i];
+		}
+	}
+	
+
+
+	private void OnMouseDown()
+	{
+		oldMousePosition = Input.mousePosition.x;
+		manualMove = true;
+	}
+
+	private void OnMouseDrag()
+	{
+		if (!stopMoving)
+		{
+			panels.transform.localPosition -= new Vector3(oldMousePosition - Input.mousePosition.x, 0, 0) * 2;
+		}
+		oldMousePosition = Input.mousePosition.x;
+		manualMove = true;
+	}
+
+	private void OnMouseUp()
+	{
+		float lastDist = 99999;
+		for (int i = 0; i < individualPanel.Count; i++)
+		{
+			if (Vector3.Distance(individualPanel[i].transform.position, transform.position) < lastDist)
+			{
+				lastDist = Vector3.Distance(individualPanel[i].transform.position, transform.position);
+				Debug.Log(lastDist);
+				index = i;
+			}
+
+		}
+		stopMoving = false;
+		manualMove = false;
+	}
+
+	public void SetIndex(int i)
+	{
+		index = i;
+	}
+
+	public void Stop()
+	{
+		stopMoving = true;
+	}
+}
