7800073d26fa68453af8a7975d9687373fbfe7a5
João Vitor <jovitor86@gmail.com>
2020-11-04 22:09:46 -0300

    cutscene e novos botoes

---
+++ Assets/Scripts/CutsceneScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Animator animator;
+    public GameObject canvas;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        WaitCutscene();
+
+    }
+
+    public void WaitCutscene()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("CutsceneAnim"))
+        {
+            canvas.SetActive(false);
+        }
+        else       
+            canvas.SetActive(true);  
+    }
+}
