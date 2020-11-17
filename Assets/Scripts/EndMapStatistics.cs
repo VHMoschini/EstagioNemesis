9a399f438a6a19779a66fe46973acff7b2efd96a
João Vitor <jovitor86@gmail.com>
2020-11-16 22:40:50 -0300

    estatisticas

---
+++ Assets/Scripts/EndMapStatistics.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndMapStatistics : MonoBehaviour
+{
+    public BarsManager barsManager;
+    public TextMeshProUGUI panelTXT;
+
+    private void OnEnable()
+    {
+        SetVictoryPanelStatistics();
+    }
+
+
+    public void SetVictoryPanelStatistics()
+    {
+        panelTXT.text = "Blocos Inimigos Destruídos: " + barsManager.hittedEnemieCubes + "\n Blocos Aliados Destruídos: " + barsManager.hittedAllyCubes;
+    }
+
+}
