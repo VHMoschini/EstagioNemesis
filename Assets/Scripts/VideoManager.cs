b09bb67d23a3cbbcd5037062efb57eb9885f354e
César Neves Mosca <Cronokami@hotmail.com>
2020-11-19 01:46:08 -0300

    Cutscene inicial por vídeo

---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+	private VideoPlayer m_VideoPlayer;
+
+	void Awake()
+	{
+		m_VideoPlayer = GetComponent<VideoPlayer>();
+		m_VideoPlayer.loopPointReached += OnMovieFinished;
+	}
+
+	void OnMovieFinished(VideoPlayer player)
+	{
+		player.Stop();
+		SceneManager.LoadScene(1);
+		Destroy(gameObject);
+		
+	}
+}
