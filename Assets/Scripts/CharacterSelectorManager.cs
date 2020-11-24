19f974c7271ff0585e91d9714774cda10a24e45f
César Neves Mosca <Cronokami@hotmail.com>
2020-11-24 18:48:13 -0300

    Personagens bloqueados e ícone de personagem ativo no carrossel

--- Assets/Scripts/CharacterSelectorManager.cs
+++ Assets/Scripts/CharacterSelectorManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class CharacterSelectorManager : MonoBehaviour
 {
     public List<GameObject> characters = new List<GameObject>();
+	private List<Image> characterImages = new List<Image>();
+	public List<Sprite> characterSprites = new List<Sprite>();
     int index;
     public Image rightButton;
     public Image leftButton;
@@ -18,10 +20,26 @@ public class CharacterSelectorManager : MonoBehaviour
 	private bool manualMove;
 	private float oldMousePosition;
 
+	public GameObject selectedCharacterPortrait;
+	private RectTransform scRect;
+	private Image scImage;
+
+	public Sprite[] characterPortraits;
 
 	private void OnEnable()
 	{
 		manualMove = false;
+		scImage = selectedCharacterPortrait.GetComponent<Image>();
+		scRect = selectedCharacterPortrait.GetComponent<RectTransform>();
+
+		for (int i = 0; i < characters.Count; i++)
+		{
+			characterImages.Add(characters[i].transform.GetChild(0).GetComponent<Image>());
+			if (PlayerPrefs.GetInt("PersonagensLiberados", 0) >= i)
+			{
+				characterImages[i].sprite = characterSprites[i];
+			}
+		}
 	}
 
 	
@@ -55,9 +73,17 @@ public class CharacterSelectorManager : MonoBehaviour
         }
 
 
-        PlayerPrefs.SetInt("playerIndex", index);
+		if (PlayerPrefs.GetInt("PersonagensLiberados", 0) >= index)
+		{
+			PlayerPrefs.SetInt("playerIndex", index);
+			scImage.sprite = characterPortraits[index];
+		}
+	}
 
-    }
+	private void LateUpdate()
+	{
+		scRect.anchoredPosition = new Vector2(Mathf.Clamp(-transform.position.x * 10 + 47, 47, 1080 + 70), scRect.anchoredPosition.y);
+	}
 
 	public void RightArrow()
     {
