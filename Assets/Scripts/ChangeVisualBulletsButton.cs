9bccf124bdebb7cb833166acdb3d0c7712e544ef
César Neves Mosca <Cronokami@hotmail.com>
2020-12-15 00:51:51 -0300

    Tirando bug que não deixa gastar bala

--- Assets/Scripts/ChangeVisualBulletsButton.cs
+++ Assets/Scripts/ChangeVisualBulletsButton.cs
@@ -7,6 +7,7 @@ public class ChangeVisualBulletsButton : MonoBehaviour
 {
     public List<Sprite> projectilesImage = new List<Sprite>();
     [HideInInspector]public int projectileIndex;
+	public PlayerCharacterManager characterManager;
 
 
     // Update is called once per frame
@@ -17,6 +18,8 @@ public class ChangeVisualBulletsButton : MonoBehaviour
 
     public void SetProjectileIndex()
     {
+		if (PlayerController.isAiming) return;
+		characterManager.SetProjectileIndex();
         if (projectileIndex < projectilesImage.Count-1)
             projectileIndex++;
         else
