84f4307f0c428a4b30baa725c96ac11b6c75c934
César Neves Mosca <Cronokami@hotmail.com>
2021-05-15 19:54:36 -0300

    Aplicando Crashlytics e ajustando erros

--- Assets/Scripts/Ads/AdInitialize.cs
+++ Assets/Scripts/Ads/AdInitialize.cs
@@ -8,6 +8,24 @@ public class AdInitialize : MonoBehaviour
     private void Start()
     {
 		MobileAds.Initialize(initStatus => { });
+
+		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+			var dependencyStatus = task.Result;
+			if (dependencyStatus == Firebase.DependencyStatus.Available)
+			{
+				// Create and hold a reference to your FirebaseApp,
+				// where app is a Firebase.FirebaseApp property of your application class.
+				var app = Firebase.FirebaseApp.DefaultInstance;
+
+				// Set a flag here to indicate whether Firebase is ready to use by your app.
+			}
+			else
+			{
+				UnityEngine.Debug.LogError(System.String.Format(
+				  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+				// Firebase Unity SDK is not safe to use here.
+			}
+		});
 	}
 	
 }
--- Assets/Scripts/Camera/CameraShowoff.cs
+++ Assets/Scripts/Camera/CameraShowoff.cs
@@ -37,23 +37,7 @@ public class CameraShowoff : MonoBehaviour
 		Instantiate(skipShowoff, Camera.main.transform.position + Camera.main.transform.forward * 3, Camera.main.transform.rotation ,Camera.main.transform);
 		SkipShowoff.script = this;
 
-		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
-			var dependencyStatus = task.Result;
-			if (dependencyStatus == Firebase.DependencyStatus.Available)
-			{
-				// Create and hold a reference to your FirebaseApp,
-				// where app is a Firebase.FirebaseApp property of your application class.
-				var app = Firebase.FirebaseApp.DefaultInstance;
-
-				// Set a flag here to indicate whether Firebase is ready to use by your app.
-			}
-			else
-			{
-				UnityEngine.Debug.LogError(System.String.Format(
-				  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-				// Firebase Unity SDK is not safe to use here.
-			}
-		});
+		
 	}
 
 	private void Update()
